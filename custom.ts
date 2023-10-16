d1251b812e02f797638095ef94952411564d00b5
Brammie15 <brammieverhulst@gmail.com>
2023-10-16 15:06:22 +0200

    fix lever

--- custom.ts
+++ custom.ts
@@ -4,15 +4,25 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
     
-    //setblock 39 68 267
-    //setblock 29 68 267
+    //lever0
+    //setblock 28 70 268
+    //lever10
+    //setblock 38 70 268
+
+    //testblock0
+    //setblock 28 67 267
+    //lever10
+    //setblock 39 67 267
     //% block="Set lever %lever value %state"
     //% lever.min=0 lever.max=10 lever.defl=0
     export function setLever(lever: number, state: boolean): void{
         let leverRoot : Position = world(29, 68, 267);
+        let testblockRoot: Position = world(28,67,267);
+
         let currentLever: Position = leverRoot.add(world(lever, 0, 0));
-        agent.teleport(leverRoot.add(world(lever, 0, 0)), NORTH);
-        if(blocks.testForBlock(IRON_BLOCK, currentLever) == state){
+        let currentTestblock: Position = testblockRoot.add(world(lever, 0, 0));
+        agent.teleport(currentLever, NORTH);
+        if (blocks.testForBlock(IRON_BLOCK, currentTestblock) == state){
             agent.interact(FORWARD);
         }
     }
