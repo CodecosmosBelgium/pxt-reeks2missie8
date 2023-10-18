46dae8e59db8c99cf27d50fa738ebd28a59a1129
Brammie15 <brammieverhulst@gmail.com>
2023-10-18 02:11:27 +0200

    Add no lever mode

--- custom.ts
+++ custom.ts
@@ -1,21 +1,39 @@
 /**
  * Custom blocks
  */
+
+enum lever_state{
+    //% block="aan"
+    ON,
+    //% block="uit"
+    OFF
+}
+
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
     
-    //lever0
-    //setblock 28 70 268
-    //lever10
-    //setblock 38 70 268
+    let currentLever = 0;
+    //% block="Zet klaar"
+    export function setup_intro(){
+        currentLever = 0;
+    }
 
-    //testblock0
-    //setblock 28 67 267
-    //lever10
-    //setblock 39 67 267
+    //% block="volgende schakelaar"
+    export function next_lever(){
+        if(currentLever < 11){
+            currentLever++;
+        }
+    }
+
+    //% block="stel schakelaar in op %state"
+    //% state.defl=lever_state.OFF
+    export function set_current_lever(state: lever_state){
+        set_lever(currentLever, state == lever_state.ON);
+    }
+
     //% block="Set lever %lever value %state"
     //% lever.min=0 lever.max=10 lever.defl=0
-    export function setLever(lever: number, state: boolean): void {
+    export function set_lever(lever: number, state: boolean): void {
         let leverRoot: Position = world(28, 70, 268);
         let testblockRoot: Position = world(28, 67, 267);
 
